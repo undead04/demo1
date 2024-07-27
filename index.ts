39a48d88cad1b02655922dcd4ac2c4b89c592f40
undead04 <antran.261004@gmail.com>
2024-07-27 21:32:51 +0700

    hello comic

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
+import hello from "./api/hello";
+
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-
+app.get('/hello',hello)
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
